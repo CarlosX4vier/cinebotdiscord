300ed43000833d9a03f2ce43bf0abdb588852100
Carlos Xavier <bombardooficial@gmail.com>
2019-06-11 09:10:52 -0300

    Organização e limpeza de codigos

--- classes/sala.js
+++ classes/sala.js
@@ -1,14 +1,51 @@
-class Sala{
+class Sala {
 
-    constructor(id, nome){
+    constructor(id, nome) {
         this.id = id;
         this.nome = nome;
         this.playlist = []
         this.tempo = 0
     }
 
-    atualizarTempo(){
+    atualizarTempo() {
         this.tempo++;
+    }
+
+    static verificarSala(salaTeste) {
+        return Sala.salas.findIndex(salaCheck => salaCheck.id == salaTeste);
+    }
+
+    /**
+     * 
+     * @param {*} idSala O id do Discord do canal
+     * @returns O index da sala na variavel Sala.salas
+     */
+    static pegarSala(idSala) {
+        return Sala.verificarSala(idSala)
+    }
+
+    static criarSala(salas, idSala) {
+        const Sala = require('./sala.js')
+        var salaUser = new Sala(idSala, "Sala numero" + idSala);
+
+        if (this.verificarSala(idSala) == -1) {
+            salas.push(salaUser);
+        }
+
+        return this.pegarSala(idSala);
+    }
+
+
+
+    async adicionarVideo(idVideo, msg) {
+        const Video = require('./video');
+        const Youtube = require('../robots/youtube');
+        var iso = require('iso8601-duration');
+
+        var r = await Youtube.main(idVideo);
+        var indexSala = Sala.verificarSala(msg.guild.id);
+        Sala.salas[indexSala].playlist.push(new Video(idVideo, r.items[0].snippet.title, iso.toSeconds(iso.parse(r.items[0].contentDetails.duration)), msg.author))
+
     }
 }
 
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = express();
 const port = 3000;
 var server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 var io = require('socket.io', { rememberTransport: false })(server);
-var iso = require('iso8601-duration');
 let ejs = require('ejs')
 const youtube = require('./robots/youtube');
 
@@ -16,21 +15,21 @@ app.set('view engine', 'ejs')
 app.set('views', './view');
 
 setInterval(() => {
-  for (i = 0; i < salas.length; i++) {
-    if (salas[i].playlist.length >= 1) {
-      if (salas[i].tempo >= salas[i].playlist[0].duracao) {
-        salas[i].tempo = 0;
-        salas[i].playlist.shift();
+  for (i = 0; i < Sala.salas.length; i++) {
+    if (Sala.salas[i].playlist.length >= 1) {
+      if (Sala.salas[i].tempo >= Sala.salas[i].playlist[0].duracao) {
+        Sala.salas[i].tempo = 0;
+        Sala.salas[i].playlist.shift();
       } else {
-        console.log("Verificacao 2.1");
-        io.to(salas[i].id).emit("time", { duracao: salas[i].tempo, sala: salas[i].id, video: salas[i].playlist[0].id });
+        Sala.salas[i].atualizarTempo();
+        io.to(Sala.salas[i].id).emit("time", { duracao: Sala.salas[i].tempo, sala: Sala.salas[i].id, video: Sala.salas[i].playlist[0].id });
       }
     }
   }
 }, 1000);
 
 
-var salas = [];
+Sala.salas = [];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -50,25 +49,29 @@ client.on('ready', () => {
 
 });
 
-function verificarSala(salaTeste) {
-  return salas.findIndex(salaCheck => salaCheck.id == salaTeste);
-}
+
 
 client.login('MzkyOTAzMTcwNTQ1ODExNDU3.XPaWwQ.J0SA8eN3y8mGEblnA6jiPB39o3U');
 
 
 client.on('message', async (msg) => {
   if (msg.content.includes("!assistir")) {
-    const Video = require('./classes/video');
 
     msg.reply("O endereço da sala do servidor '" + msg.guild.name + "' é " + credentials.site + "/?sala=" + msg.guild.id);
 
+
+    /* salaUser = new Sala(salaID, "Sala numero" + salaID);
+      if (verificarSala(salaUser.id) == -1) {
+        Sala.salas.push(salaUser);
+      }*/
+
+    var idSala = Sala.criarSala(Sala.salas, msg.guild.id);
+
     var url = new URL(msg.content.split(" ")[1]);
     var idVideo = url.searchParams.get('v');
-    r = await youtube.main(idVideo);
-    //console.log(iso.toSeconds(iso.parse(r.items[0].contentDetails.duration)));
-    salas[verificarSala(msg.guild.id)].playlist.push(new Video(idVideo, r.items[0].snippet.title, iso.toSeconds(iso.parse(r.items[0].contentDetails.duration)), msg.author))
-    console.log(salas[verificarSala(msg.guild.id)]);
+    await Sala.salas[idSala].adicionarVideo("yYzaEnt0kxs", msg);
+    await Sala.salas[idSala].adicionarVideo("SEmlPFYAl-g", msg);
+    await Sala.salas[idSala].adicionarVideo(idVideo, msg);
   }
 });
 
@@ -91,16 +94,13 @@ app.get('/', (req, res) => {
 
 io.on('connection', function (socket) {
   let salaID = socket.handshake.query.sala;
-  salaUser = new Sala(salaID, "Sala numero" + salaID);
 
-  socket.join(salaUser.id, function () {
-    console.log("enviado para sala: " + salaUser.id);
+  socket.join(salaID, function () {
+    console.log("enviado para sala: " + salaID);
 
     console.log('a user connected');
-    console.log(verificarSala(salaUser.id))
-    if (verificarSala(salaUser.id) == -1) {
-      salas.push(salaUser);
-    }
+    console.log(Sala.verificarSala(salaID))
+
   });
 
 });
